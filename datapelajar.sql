69e1f455f0696afe062a4af758ad6b21740b5f70
MUHAMMAD ALIF DANIEL <alifhaker1@gmail.com>
2023-01-12 20:18:12 +0800

    Add files via upload

--- datapelajar.sql
+++ datapelajar.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2023 at 04:53 AM
+-- Generation Time: Jan 12, 2023 at 01:17 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,17 +33,6 @@ CREATE TABLE `contact_pelajar` (
   `alamat` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `contact_pelajar`
---
-
-INSERT INTO `contact_pelajar` (`no_ndp`, `emel`, `alamat`) VALUES
-(23221084, 'aina@gmail.com', 'Lot 1204, Belakang Masjid Palekbang, 16040, Tumpat, Kelantan.'),
-(23221100, 'ishu@gmail.com', 'No 43 Jalan Pualam 7/32, Seksyen 7, 40000, Shah Alam, Selangor'),
-(23221102, 'najwa@gmail.com', 'No 1A Kampung Sungai Dadeh, Temoh Stesen, 35350, Temoh, Perak'),
-(23221132, 'faisal@gmail.com', '53 Laluan Inton Kanthan Desa 3 Intan Kanthan, 3120, Chemor, Perak.'),
-(23221157, 'allyssa@gmail.com', 'Lot 1033 No 8 Lorong 4 Kg Merbau Sempak, 47000, Sungai Buloh, Selangor');
-
 -- --------------------------------------------------------
 
 --
@@ -54,20 +43,10 @@ CREATE TABLE `info_pelajar` (
   `id` int(3) NOT NULL,
   `no_ndp` int(9) NOT NULL,
   `nama_pelajar` varchar(50) NOT NULL,
-  `no_kp` varchar(50) NOT NULL,
+  `no_kp` int(50) NOT NULL,
   `jantina` varchar(50) NOT NULL,
   `no_hp` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `info_pelajar`
---
-
-INSERT INTO `info_pelajar` (`id`, `no_ndp`, `nama_pelajar`, `no_kp`, `jantina`, `no_hp`) VALUES
-(1, 23221084, 'AINA ATIKAH BINTI MOHD AZHAR', '20828160168', 'Perempuan', '0136793937'),
-(2, 23221102, 'AINA NAJWA BINTI AMRAN', '21013031452', 'Perempuan', '0173693223'),
-(3, 23221157, 'ALLYSSA BINTI ABDULLAH', '31206140104', 'Perempuan', '0188746883'),
-(5, 23221100, 'ISYUAARIYAA A/P KRISHNAN', '30526141256', 'Perempuan', '0133367699');
 
 --
 -- Indexes for dumped tables
@@ -90,10 +69,16 @@ ALTER TABLE `info_pelajar`
 --
 
 --
+-- AUTO_INCREMENT for table `contact_pelajar`
+--
+ALTER TABLE `contact_pelajar`
+  MODIFY `no_ndp` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23221148;
+
+--
 -- AUTO_INCREMENT for table `info_pelajar`
 --
 ALTER TABLE `info_pelajar`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
